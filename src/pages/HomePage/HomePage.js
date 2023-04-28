28c9a898e7534b93826c4433603348f0f9d25f57
anarehder <anapaula.rehder@gmail.com>
2023-04-28 16:28:18 -0300

    feat/exeibirProdutosBranco

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,14 +1,41 @@
 import Header from "../../components/Header/Header";
 import { ProductsContainer, ProductCard, ButtonBuy } from "./StyledHomePage.js";
-import products from "../../constants/productsExample.js";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Cart } from "../../contexts/CartContext";
+import axios from "axios";
 
 export default function HomePage() {
 
   const [cartItems, setCartItems] = useContext(Cart);
+  const [products, setProducts] = useContext(Cart);
+  const [carregando, setCarregando] = useState(false);
+
   // PARA USAR QUANDO FOR CONECTAR COM O BACKEND
   //const promise = axios.get(`${process.env.REACT_APP_API_URL}/operations`, config);
+
+  useEffect(() => {
+    setCarregando(true);
+    const promise = axios.get(`${process.env.REACT_APP_API_URL}/`);
+    promise.then(resposta => {
+      console.log(resposta.data)
+      setProducts(resposta.data);
+      setCarregando(false);
+    })
+    promise.catch(erro => {
+      alert(erro.response.data);
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (carregando === true) {
+    return (
+      <>
+        <Header>
+          CARREGANDO...
+        </Header>
+      </>
+    )
+  }
 
   function addToCart(product) {
     if (cartItems.some(element => element.id === product._id)) {
@@ -19,8 +46,10 @@ export default function HomePage() {
       })
       setCartItems(cartItems);
     } else {
-      const newItem = { id: product._id, amount: 1,
-        name: product.name, image: product.image, price: product.price};
+      const newItem = {
+        id: product._id, amount: 1,
+        name: product.name, image: product.image, price: product.price
+      };
       setCartItems([...cartItems, newItem]);
     }
   }
@@ -34,7 +63,7 @@ export default function HomePage() {
             <img src={product.image} alt="produto" />
             <div>
               <span>{product.name}</span>{" "}
-              <span>R$ {product.price.toFixed(2)}</span>
+              <span>R$ {Number(product.price).toFixed(2)}</span>
             </div>
             <div>{product.description}</div>
             <ButtonBuy onClick={() => addToCart(product)}>COMPRE</ButtonBuy>
